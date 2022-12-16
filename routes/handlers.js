a6a419be0f89293a1aeaab6b90ec47adeafd733c
Eric Reed <mail@ericreed.me>
2022-12-16 00:23:41 -0500

    export handlers

---
+++ routes/handlers.js
@@ -0,0 +1,23 @@
+exports.handleAboveBelow = (numbers, comp) => {
+
+    let above = 0;
+    let below = 0;
+
+    //Spec doesn't say what to do if equal, intentionally ignoring case and asking about it
+    numbers.forEach((num) => {
+        if (num > comp) {
+            above++
+        } else if (num < comp) {
+            below++
+        }
+    })
+    return({above: above, below: below})
+}
+
+exports.stringRotation = (stringToRot, rotateAmount) => {
+    let initString = stringToRot;
+    let modRotAount  = initString.length - (rotateAmount % initString.length)
+  
+    let stringRes = initString.slice(modRotAount) + initString.slice(0, modRotAount)  
+    return({ stringRes: stringRes })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,19 @@
 var express = require('express');
+const { handleAboveBelow, stringRotation } = require('./handlers');
 var router = express.Router();
 
 
-router.post('/aboveBelow', function(req, res, next) {
-  const body = req.body  
-  
-  let comp = req.body.comparator;
-  let above = 0;
-  let below = 0;
-
-  //Spec doesn't say what to do if equal, intentionally ignoring case and asking about it
-  body.numbers.forEach((num) => {
-    if(num > comp){
-      above++
-    } else if (num < comp){
-      below++
-    }
-  }) 
-  res.status(200).json({ above: above, below: below })
+router.post('/aboveBelow', function (req, res, next) {
+    let numbers = req.body.numbers;
+    let comp = req.body.comparator;
+    let result = handleAboveBelow(numbers, comp);
+    res.status(200).json(result)
 });
 
 
-router.post('/stringRotation', function(req, res, next) {
-  const body = req.body  
-
-  let initString = body.stringToRot;
-  let modRotAount  = initString.length - (body.rotateAmount % initString.length)
-
-  let stringRes = initString.slice(modRotAount) + initString.slice(0, modRotAount)  
-
-  res.status(200).json({ stringRes: stringRes })
+router.post('/stringRotation', function (req, res, next) {
+    let stringRes = stringRotation(req.body.stringToRot, req.body.rotateAmount);
+    res.status(200).json(stringRes)
 });
 
 
